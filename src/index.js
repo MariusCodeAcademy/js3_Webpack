37d9874742fccdf5c23aa64c4e7495c32e04c495
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-20 09:48:29 +0300

    import expor fn

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-console.log('index.js')
+import add from "./js/add.js";
+import say from "./js/say.js";
+console.log("index.js");
+
+console.log(" add(1, 2, 3) = ", add(1, 2, 3));
+
+console.log(' say("nice day")', say("nice day"));
